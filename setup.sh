c5d6cd73e24a23f449ef7ffaf4fdd6277df5ff2c
runNlearn <jsm21star@gmail.com>
2021-02-12 03:06:33 +0900

    add comments

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 !#/usr/bin/env
 
+echo Install jpeg2dct
 git clone https://github.com/uber-research/jpeg2dct.git
 sed -i s/\<string\>/\<tstring\>/g jpeg2dct/jpeg2dct/tensorflow/tf_lib.cc
 sed -i '52,59d' jpeg2dct/jpeg2dct/tensorflow/__init__.py
@@ -7,8 +8,11 @@ sed -i '158,160d' jpeg2dct/jpeg2dct/common/dctfromjpg.cc  # delete `transcode`
 cd jpeg2dct && python3 setup.py install
 cd ..
 
+echo Install jpegio
 git clone https://github.com/dwgoon/jpegio.git
 cd jpegio && python3 setup.py install
 cd ..
 
+echo Install simplejpeg
 pip install simplejpeg &> /dev/null
+
