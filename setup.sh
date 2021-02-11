8a2830d1fc53dae4a8d14ba45ccd8ec80cb156a3
runNlearn <jsm21star@gmail.com>
2021-02-12 03:56:51 +0900

    delete repo after installation

--- setup.sh
+++ setup.sh
@@ -7,11 +7,13 @@ sed -i '52,59d' jpeg2dct/jpeg2dct/tensorflow/__init__.py
 sed -i '158,160d' jpeg2dct/jpeg2dct/common/dctfromjpg.cc  # delete `transcode`
 cd jpeg2dct && python3 setup.py install
 cd ..
+rm -rf jpeg2dct
 
 echo Install jpegio
 git clone https://github.com/dwgoon/jpegio.git
 cd jpegio && python3 setup.py install
 cd ..
+rm -rf jpegio
 
 echo Install simplejpeg
 pip install simplejpeg &> /dev/null
