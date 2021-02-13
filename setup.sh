c17ac7f94a1596b52c2ed71d421f7de267db9357
runNlearn <jsm21star@gmail.com>
2021-02-13 15:24:24 +0900

    refactor code to run on both macos & linux

--- setup.sh
+++ setup.sh
@@ -1,14 +1,27 @@
-!#/usr/bin/env
 
-echo Install jpeg2dct
-git clone https://github.com/uber-research/jpeg2dct.git
-sed -i s/\<string\>/\<tstring\>/g jpeg2dct/jpeg2dct/tensorflow/tf_lib.cc
-sed -i '52,59d' jpeg2dct/jpeg2dct/tensorflow/__init__.py
-sed -i '158,160d' jpeg2dct/jpeg2dct/common/dctfromjpg.cc  # delete `transcode`
-cd jpeg2dct && python3 setup.py install
-cd ..
-rm -rf jpeg2dct
+function install_jpeg2dct() {
+    echo "Install jpeg2dct"
+    git clone https://github.com/uber-research/jpeg2dct.git
+    os=$(uname)
+    if [ $os = "Darwin" ]
+    then
+        sed -i ''  s/\<string\>/\<tstring\>/g jpeg2dct/jpeg2dct/tensorflow/tf_lib.cc
+        sed -i '' '52,59d' jpeg2dct/jpeg2dct/tensorflow/__init__.py
+        sed -i '' '158,160d' jpeg2dct/jpeg2dct/common/dctfromjpg.cc  
+    elif [ $os = "Linux" ]
+    then
+        sed -i s/\<string\>/\<tstring\>/g jpeg2dct/jpeg2dct/tensorflow/tf_lib.cc
+        sed -i '52,59d' jpeg2dct/jpeg2dct/tensorflow/__init__.py
+        sed -i '158,160d' jpeg2dct/jpeg2dct/common/dctfromjpg.cc  
+    fi
 
-echo 'Install simplejpeg & pyyaml==3.13'
-pip3 install simplejpeg PyYAML==5.3.1 
+    cd jpeg2dct && python3 setup.py install
+    cd ..
+    rm -rf jpeg2dct
+}
 
+
+install_jpeg2dct()
+
+echo 'Install simplejpeg & pyyaml'
+pip3 install simplejpeg PyYAML==5.3.1
